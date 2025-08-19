cab91cb6bea85fda5ca7020c8ccdc6fdf285b2cc
dalecb13 <dalecb13@gmail.com>
2025-08-19 19:36:01 +0200

    added initial create product form

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import AddProductForm from '@/components/AddProductForm';
+import { ArrowLeft } from 'lucide-react';
+import { useRouter } from "next/navigation";
+
 const AddProductPage = () => {
-  <div className="min-h-screen flex flex-col">
-    
-  </div>
+  const router = useRouter();
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="flex items-center p-8 w-full">
+        <ArrowLeft size={20} onClick={() => router.back()} className='cursor-pointer hover:text-gray-500' />
+        <h1 className='text-lg ml-2'>Add Product</h1>
+      </div>
+
+      <AddProductForm />
+    </div>
+  )
 }
 
 export default AddProductPage;
