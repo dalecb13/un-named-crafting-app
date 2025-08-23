10c267d2671b4edffb7c7dd845af2ee5bc5e56f0
dalecb13 <dalecb13@gmail.com>
2025-08-23 21:38:16 +0200

    added initial create product endpoint

--- data-access/products.ts
+++ data-access/products.ts
@@ -1,4 +1,4 @@
-import { GetProductError } from "@/utils/error";
+import { AddProductError, GetProductError } from "@/utils/error";
 import { supabase } from "@/utils/supabase";
 
 export type Product = {
@@ -10,9 +10,19 @@ export type Product = {
   ownerId: string;
 }
 
+export type AddProductDto = {
+  productName: string;
+  productDescription: string;
+  tags: string[];
+  numberOfUnits: number;
+  ownerId: string;
+}
+
+const PRODUCTS_TABLE = 'products';
+
 const getProducts = async(): Promise<Product[]> => {
   const { data, error } = await supabase
-    .from('products')
+    .from(PRODUCTS_TABLE)
     .select("*");
 
   if (error) { throw new GetProductError(`Error getting your products: ${error}`); }
@@ -20,4 +30,23 @@ const getProducts = async(): Promise<Product[]> => {
   return data;
 }
 
-export { getProducts }
+const addProduct = async (dto: AddProductDto) => {
+  const { data, error } = await supabase
+    .from(PRODUCTS_TABLE)
+    .insert({
+      product_name: dto.productName,
+      product_description: dto.productDescription,
+      tags: dto.tags,
+      number_of_units: dto.numberOfUnits,
+      owner_id: dto.ownerId,
+    });
+
+  if (error) {
+    console.error('Error adding your product:', error);
+    throw new AddProductError(`Error adding your product: ${error}`)
+  }
+
+  return data;
+}
+
+export { getProducts, addProduct }
