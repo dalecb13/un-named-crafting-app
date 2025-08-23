13efec213bf9b7ac6b5be5c5edfcb15edf064f73
dalecb13 <dalecb13@gmail.com>
2025-08-23 21:31:06 +0200

    added initial fetch for products

---
+++ data-access/products.ts
@@ -0,0 +1,22 @@
+import { GetProductError } from "@/utils/error";
+import { supabase } from "@/utils/supabase";
+
+export type Product = {
+  productName: string;
+  productDescription: string;
+  tags: string[];
+  numberOfUnits: number;
+  ownerId: string;
+}
+
+const getProducts = async(): Promise<Product[]> => {
+  const { data, error } = await supabase
+    .from('products')
+    .select("*");
+
+  if (error) { throw new GetProductError(`Error getting your products: ${error}`); }
+
+  return data;
+}
+
+export { getProducts }
